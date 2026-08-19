1146cfde66a6200950642bbf4566b38ddf446f07
Dellos12 <DellosDevOps@outlook.com>
2026-08-18 22:58:32 -0300

    chore: formatando arquivos terraform2

--- main.tf
+++ main.tf
@@ -23,19 +23,17 @@ resource "aws_vpc" "vpc_economia" {
 
 # A ENGRENAGEM CORRIGIDA E BLINDADA
 resource "aws_security_group" "sg_auditado" {
-  # ✅ AJUSTE 1: Evita colisão de nomes adicionando um sufixo aleatório automático
   name_prefix = "sg-vulneravel-auditoria-"
   description = "Security Group de teste para validacao do OPA"
   vpc_id      = aws_vpc.vpc_economia.id
 
   # ✅ CONFORMIDADE ATENDIDA (SOC 2 / PCI-DSS)
-  # O acesso SSH (22) agora está restrito APENAS para IPs internos da nossa VPC (10.0.0.0/16)
   ingress {
     description = "Acesso SSH restrito a rede interna"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.vpc_economia.cidr_block]
+    cidr_blocks = [aws_vpc.vpc_economia.cidr_block] 
   }
 
   egress {
@@ -45,6 +43,5 @@ resource "aws_security_group" "sg_auditado" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ✅ AJUSTE 2: Garante que a VPC nasça 100% antes de tentar ler o CIDR block dela
   depends_on = [aws_vpc.vpc_economia]
 }
