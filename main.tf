2bc2e029c01f0f4505501df1ff52b99ad3a54977
Dellos12 <DellosDevOps@outlook.com>
2026-08-18 22:46:04 -0300

    🛡️ security: restringindo acesso SSH para conformidade SOC 2/PCI

--- main.tf
+++ main.tf
@@ -21,26 +21,26 @@ resource "aws_vpc" "vpc_economia" {
   }
 }
 
-# A ENGRENAGEM CRÍTICA: O Security Group (A nossa muralha)
+# A ENGRENAGEM CORRIGIDA: O Security Group Protegido
 resource "aws_security_group" "sg_auditado" {
   name        = "sg-vulneravel-auditoria"
   description = "Security Group de teste para validacao do OPA"
   vpc_id      = aws_vpc.vpc_economia.id
 
-  # ❌ VIOLAÇÃO CRÍTICA DE SEGURANÇA (SOC 2 CC6.1 / PCI-DSS 1.2)
-  # Este bloco abre a porta SSH (22) para QUALQUER IP do planeta terra (0.0.0.0/0)
+  # ✅ CONFORMIDADE ATENDIDA (SOC 2 / PCI-DSS)
+  # O acesso SSH (22) agora está restrito APENAS para IPs internos da nossa VPC (10.0.0.0/16)
   ingress {
-    description      = "Acesso SSH público"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] 
+    description = "Acesso SSH restrito a rede interna"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc_economia.cidr_block] # Utiliza dinamicamente o CIDR da VPC
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
